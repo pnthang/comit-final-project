46194f6f066803e56dc735531cc3caac303e9aa6
namtranhoai <namtranhoai@gmail.com>
2019-11-17 04:08:45 -0600

    'init'

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  function foo(
+    a,
+    b,
+    c,
+    d,
+    ...) {
+    return a;
+  }
   return (
     <div className="App">
-     <h1>ComIT final project</h1>
+      <h1>ComIT final project</h1>
     </div>
   );
 }
