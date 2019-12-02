595766eef00749ec5a00f425edb3792cf7088715
namtranhoai <namtranhoai@gmail.com>
2019-12-02 10:51:47 -0600

    Updates

---
+++ precache-manifest.16715ef92863d2d8f8196aa1f27c630f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6532422176ffe3f2e5054bca57f1f8a5",
+    "url": "/comit-final-project/index.html"
+  },
+  {
+    "revision": "1e41d09c1b09285ca025",
+    "url": "/comit-final-project/static/css/main.728a6410.chunk.css"
+  },
+  {
+    "revision": "f28ac92141879ac4a0f4",
+    "url": "/comit-final-project/static/js/2.f7c1ea11.chunk.js"
+  },
+  {
+    "revision": "1e41d09c1b09285ca025",
+    "url": "/comit-final-project/static/js/main.b9a9f71f.chunk.js"
+  },
+  {
+    "revision": "5a04b1db535aac1da1c4",
+    "url": "/comit-final-project/static/js/runtime-main.7b1809e5.js"
+  }
+]);
