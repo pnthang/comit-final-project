9e3afa2299fad2434ad66fd13f7e3ff802e3ebff
namtranhoai <namtranhoai@gmail.com>
2019-12-17 20:30:12 -0600

    Updates

--- precache-manifest.720bf8848b059ae46cbaa9f51d999011.js
+++ precache-manifest.720bf8848b059ae46cbaa9f51d999011.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "22a3fc061607e5fe2f40b0d5ed4ed2e1",
+    "revision": "426a9e2b4f948475ba36a21109e0cfa6",
     "url": "/comit-final-project/index.html"
   },
   {
-    "revision": "10f20ea2dccc938bfe1c",
+    "revision": "7c5b2f414afeaccb5024",
     "url": "/comit-final-project/static/css/2.ff1a2a71.chunk.css"
   },
   {
-    "revision": "65dc3b6d76304fdea690",
+    "revision": "c1c6d927466c984f23d6",
     "url": "/comit-final-project/static/css/main.ba006cf1.chunk.css"
   },
   {
-    "revision": "10f20ea2dccc938bfe1c",
-    "url": "/comit-final-project/static/js/2.c2a8eb03.chunk.js"
+    "revision": "7c5b2f414afeaccb5024",
+    "url": "/comit-final-project/static/js/2.da3d6c18.chunk.js"
   },
   {
     "revision": "10816ad15893520f3fedffc48f8d2203",
-    "url": "/comit-final-project/static/js/2.c2a8eb03.chunk.js.LICENSE"
+    "url": "/comit-final-project/static/js/2.da3d6c18.chunk.js.LICENSE"
   },
   {
-    "revision": "65dc3b6d76304fdea690",
-    "url": "/comit-final-project/static/js/main.1e47fc17.chunk.js"
+    "revision": "c1c6d927466c984f23d6",
+    "url": "/comit-final-project/static/js/main.7b52efd3.chunk.js"
   },
   {
     "revision": "06b94d0c3fc1ef9f1262",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/comit-final-project/precache-manifest.9c3715c2645ce46829461283bced32a7.js"
+  "/comit-final-project/precache-manifest.720bf8848b059ae46cbaa9f51d999011.js"
 );
 
 self.addEventListener('message', (event) => {
